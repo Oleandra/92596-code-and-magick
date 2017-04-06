e4e2bb7360ae5c74db77371504a871c14bd9d006
Olga <olga.verhovetchi@gmail.com>
2017-04-05 23:08:01 -0500

    reindent

--- js/stats.js
+++ js/stats.js
@@ -1,53 +1,53 @@
-'use strict'
+'use strict';
 window.renderStatistics = function (ctx, names, times) {
-    ctx.fillStyle='rgba(0, 0, 0, 0.7)';
-    ctx.strokeRect(110, 20, 420, 270);
-    ctx.fillRect(110, 20, 420, 270);
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.strokeRect(110, 20, 420, 270);
+  ctx.fillRect(110, 20, 420, 270);
 
-    ctx.fillStyle = 'rgba(256, 256, 256, 1.0)';
-    ctx.strokeRect(100, 10, 420, 270);
-    ctx.fillRect(100, 10, 420, 270);
+  ctx.fillStyle = 'rgba(256, 256, 256, 1.0)';
+  ctx.strokeRect(100, 10, 420, 270);
+  ctx.fillRect(100, 10, 420, 270);
 
+  ctx.fillStyle = '#000';
+  ctx.font = '16px PT Mono';
+
+  ctx.fillText('Ура вы победили!', 120, 40);
+
+  var max = -1;
+  var maxName;
+
+  for (i = 0; i < times.length; i++) {
+    var current = times[i];
+    if (current > max) {
+      max = current;
+      maxName = names[i];
+    }
+  }
+
+  var histogramHeight = 150; // px;
+  var step = histogramHeight / (max - 0); // px;
+
+  ctx.fillText('Худшее время: ' + max.toFixed(2) + 'мс у игрока ' + maxName, 120, 60);
+
+  var barWidth = 40; // px;
+  var indent = 50; // px;
+  var initialX = 128; // px;
+  var initialY = 100; // px;
+
+  ctx.textBaseline = 'bottom'; // положение надписи от левого верхнего угла
+
+  for (var i = 0; i < times.length; i++) {
+    var coordY = initialY + histogramHeight - times[i] * step;
+    var coordX = initialX + i * barWidth + i * indent;
+
+    if (names[i] === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1.0)';
+    } else {
+      ctx.fillStyle = 'rgba(0, 0, 255, ' + Math.random() + ')';
+    }
+    ctx.fillRect(coordX, coordY, barWidth, times[i] * step);
     ctx.fillStyle = '#000';
-    ctx.font = '16px PT Mono';
-
-    ctx.fillText('Ура вы победили!', 120, 40);
-
-    var max = -1;
-    var maxName;
-
-    for (i = 0; i < times.length; i++) {
-        var current = times[i];
-        if (current > max) {
-            max = current;
-            maxName=names[i];
-        }
-    }
-
-    var histogramHeight = 150;              // px;
-    var step = histogramHeight / (max - 0); // px;
- 
-    ctx.fillText('Худшее время: ' + max.toFixed(2) + 'мс у игрока ' + maxName, 120, 60);
-
-    var barWidth = 40; // px;
-    var indent = 50;    // px;
-    var initialX = 128; // px;
-    var initialY = 100;  // px;
-
-    ctx.textBaseline = 'bottom'; // положение надписи от левого верхнего угла
-    
-    for(var i = 0; i < times.length; i++) {
-        var coordY =   initialY + histogramHeight - times[i]*step;
-        var coordX =  initialX + i*barWidth + i*indent;
-        
-        if (names[i]==='Вы') {
-           ctx.fillStyle = 'rgba(255, 0, 0, 1.0)'; 
-        } else {
-            ctx.fillStyle ='rgba(0, 0, 255, '+Math.random()+')';
-        }
-        ctx.fillRect(coordX, coordY, barWidth, times[i] * step);
-        ctx.fillStyle = '#000';
-        ctx.fillText(times[i].toFixed(0), coordX, coordY);
-        ctx.fillText(names[i], coordX, initialY+histogramHeight+20);
-    }
+    ctx.fillText(times[i].toFixed(0), coordX, coordY);
+    ctx.fillText(names[i], coordX, initialY + histogramHeight + 20);
+  }
 };
