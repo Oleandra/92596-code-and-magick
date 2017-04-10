1340a4b88f3b41cee680082bf1c37a8841f64fc3
Olga <olga.verhovetchi@gmail.com>
2017-04-09 21:46:36 -0500

    created setup.js and randomly created wizards+append doc to index.html

---
+++ js/setup.js
@@ -0,0 +1,47 @@
+'use strict';
+var userProfile = document.querySelector('.setup');
+userProfile.classList.remove('hidden');
+
+var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var FAMILY_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
+var personsLook = [];
+
+
+for (var i = 0; i < 4; i++) {
+  var person = {
+    name: NAMES[Math.floor((Math.random() * NAMES.length) + 0)] + ' ' + FAMILY_NAMES[Math.floor((Math.random() * FAMILY_NAMES.length))],
+    coatColor: coatColors[Math.floor((Math.random() * coatColors.length))],
+    eyesColor: eyesColors[Math.floor((Math.random() * eyesColors.length))],
+
+  }
+
+  personsLook.push(person);
+
+};
+var userDialog = document.querySelector('.setup');
+userDialog.classList.remove('hidden');
+
+var similarListElement = userDialog.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
+
+var renderWizard = function(p) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+
+  wizardElement.querySelector('.setup-similar-label').textContent = p.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = p.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = p.eyesColor;
+
+  return wizardElement;
+}
+
+var fragment = document.createDocumentFragment();
+
+for (var i = 0; i < personsLook.length; i++) {
+  var item = personsLook[i];
+  fragment.appendChild(renderWizard(item))
+}
+similarListElement.appendChild(fragment);
+
+userDialog.querySelector('.setup-similar').classList.remove('hidden');
